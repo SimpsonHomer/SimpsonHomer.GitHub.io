bcab838089463c6707891226608c708fa09cb5c7
Corey McGrath <cmcgrat1@gmail.com>
2014-03-28 20:20:03 -0700

    navigation

--- navigate.js
+++ navigate.js
@@ -1,22 +1,10 @@
-// when the document is ready, run this function
-jQuery(function( $ ) {
-    var keymap = {};
+addListener(window, "keypress", function(key) {
+key = key || window.event;
+var theKey = key.which || key.keyCode;
 
-    // LEFT
-    keymap[ 37 ] = "#prev";
-    // RIGHT
-    keymap[ 39 ] = "#next";
-
-    $( document ).on( "keyup", function(event) {
-        var href,
-            selector = keymap[ event.which ];
-        // if the key pressed was in our map, check for the href
-        if ( selector ) {
-            href = $( selector ).attr( "href" );
-            if ( href ) {
-                // navigate where the link points
-                window.location = href;
-            }
-        }
-    });
-});
+switch (theKey) {
+  case 37 :
+    window.location = "#prev";
+  case 39 :
+    window.location = "#next";
+}
