60d1237005f33e1aaa0a4909f3b6bbd97438f874
Corey McGrath <cmcgrat1@gmail.com>
2014-03-29 13:59:12 -0700

    ???

--- navigate.js
+++ navigate.js
@@ -1,21 +1,34 @@
 <SCRIPT LANGUAGE="JavaScript">
 <!--
 
-function nav() {
+<script>
+function addListener(element, type, response) {
+        if (element.addEventListener) {
+            element.addEventListener(type, response, false);
+        }
+        else if (element.attachEvent) {
+            element.attachEvent("on" + type, response);
+        }
+    }
+    addListener(window, "keypress", function(key) {
+        // do this stuff when a key is pressed:
 
-event = event || window.event;
-  var charCode = event.charCode;
-  if (charCode == undefined || charCode === 0)
-    charCode = event.keyCode;
+        // http://stackoverflow.com/a/5420482
+        key = key || window.event;
+        var theKey = key.which || key.keyCode;
 
-if charCode == "37"
-  window.location.href = "#prev";
-
-if charCode == "39"
-  window.location.href = "#next";
-}
-
-element.addEventListener('click', nav() , false);
+        switch (theKey) {
+            // if they hit 37
+        case 37 :
+            window.location.href = "/links.html";
+            break;
+            // if they press 39
+            case 39 :
+                window.location.href = "/about.html";
+                break;
+        }
+    });
+</script>
 
 -->
 </SCRIPT>
