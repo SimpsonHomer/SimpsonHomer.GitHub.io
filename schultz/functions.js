166d251cc4440b65048300020bef6ddff71febf6
Corey McGrath <cmcgrat1@gmail.com>
2015-02-08 20:32:34 -0800

    susanna's page

--- schultz/functions.js
+++ schultz/functions.js
@@ -1,6 +1,5 @@
 function clear() {
 document.getElementById('about').style.display = 'none';
-document.getElementById('teaching').style.display = 'none';
 document.getElementById('research').style.display = 'none';
 document.getElementById('cv').style.display = 'none';
 document.getElementById('contact').style.display = 'none';
@@ -8,23 +7,13 @@ document.getElementById('contact').style.display = 'none';
 
 function about() {
 	document.getElementById('about').style.display = 'block';
-	document.getElementById('teaching').style.display = 'none';
 	document.getElementById('research').style.display = 'none';
 	document.getElementById('cv').style.display = 'none';
 	document.getElementById('contact').style.display = 'none';
 }
 
-function teaching() {
-document.getElementById('teaching').style.display = 'block';
-document.getElementById('about').style.display = 'none';
-document.getElementById('research').style.display = 'none';
-document.getElementById('cv').style.display = 'none';
-document.getElementById('contact').style.display = 'none';
-}
-
 function research() {
 	document.getElementById('about').style.display = 'none';
-	document.getElementById('teaching').style.display = 'none';
 document.getElementById('research').style.display = 'block';
 document.getElementById('cv').style.display = 'none';
 document.getElementById('contact').style.display = 'none';
@@ -32,7 +21,6 @@ document.getElementById('contact').style.display = 'none';
 
 function cv() {
 	document.getElementById('about').style.display = 'none';
-	document.getElementById('teaching').style.display = 'none';
 	document.getElementById('research').style.display = 'none';
 document.getElementById('cv').style.display = 'block';
 document.getElementById('contact').style.display = 'none';
@@ -40,7 +28,6 @@ document.getElementById('contact').style.display = 'none';
 
 function contact() {
 	document.getElementById('about').style.display = 'none';
-	document.getElementById('teaching').style.display = 'none';
 	document.getElementById('research').style.display = 'none';
 	document.getElementById('cv').style.display = 'none';
 document.getElementById('contact').style.display = 'block';
